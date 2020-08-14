8eb832fd610c3b436dfcaf45520c79b9ad69621f
CanoMaria <mcano596@alumnos.iua.edu.ar>
2020-08-14 18:11:51 -0300

    Proyecto del profe con dependencias

--- src/main/java/ar/edu/iua/BackendApplication.java
+++ src/main/java/ar/edu/iua/BackendApplication.java
@@ -2,12 +2,19 @@ package ar.edu.iua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.json.*;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+		
+		JSONObject myObject = new JSONObject();
+
+        // Cadenas de texto básicas
+        myObject.put("name","Carlos");
+        System.out.println(myObject.get("name"));
 	}
 
 }
